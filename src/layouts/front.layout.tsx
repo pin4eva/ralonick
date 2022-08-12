149fa5010fd160f2e359154d88a5404705408b7d
Eferhime Avhurhi <efeluckybouy@gmail.com>
2022-08-12 13:53:59 +0100

    first comit

--- src/layouts/front.layout.tsx
+++ src/layouts/front.layout.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import BodyComp from "../components/BodyComp";
 import FooterComp from "../components/FooterComp";
 import HeaderComp from "../components/HeaderComp";
+
+
 
 const FrontLayout: React.FC<{ children: React.ReactElement }> = ({ children }) => {
 	return (
 		<div className="app">
 			<HeaderComp />
 			<main className="app-children">{children}</main>
+			<BodyComp />
 			<FooterComp />
 		</div>
 	);

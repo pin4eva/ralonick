40f8060ffc1acfa35649fcd33574f90ce58f3d5b
oluwa-tobi <oluwatobi@yahoo.com>
2022-08-19 13:14:28 +0100

    added first section of the about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,7 +5,17 @@ const AboutPage = () => {
 	return (
 		<FrontLayout>
 			<div className="about-page">
-				<div>1</div>
+				<div className="first-section">
+					<div className="frame1">
+					<img src="assets/about1.png" alt="frame1" />
+					</div>
+					<div className="first-text fw-800">
+						<p>About </p>
+						<p className="p2">Ralonik LTD.</p>
+					</div>
+				</div>
+				{/*========== END OF SECTION ONE ==========*/}
+
 				<div>2</div>
 				<div>3</div>
 				<div>4</div>

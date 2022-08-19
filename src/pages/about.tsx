2f4cd0acd0aeeb1412305e9f04f45847e4cc6568
oluwa-tobi <oluwatobi@yahoo.com>
2022-08-19 16:57:46 +0100

    added fourth section of the about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,10 +4,10 @@ import FrontLayout from "../layouts/front.layout";
 const AboutPage = () => {
 	return (
 		<FrontLayout>
-			<div className="about-page">
+			<div className="about-page container">
 				<div className="first-section">
 					<div className="frame1">
-					<img src="assets/about1.png" alt="frame1" />
+						<img src="assets/about1.png" alt="frame1" />
 					</div>
 					<div className="first-text fw-800">
 						<p>About </p>
@@ -16,9 +16,46 @@ const AboutPage = () => {
 				</div>
 				{/*========== END OF SECTION ONE ==========*/}
 
-				<div>2</div>
+				<div className="second-section">
+					
+				</div>
 				<div>3</div>
-				<div>4</div>
+
+				{/* ===========beginning of fourth section============= */}
+				<div className="fourth-section container">
+					<div className="frame3">
+						<img src="assets/frame3.png" alt="frame2" />
+					</div>
+					<div className="fourth-section-text1">
+						<span>Our Vision:</span>
+						<ul>
+							<li>We aim to be Nigeria’s leading company in Design,  Engineering, Procurement, Fabrication, Construction, Equipment Leasing  and Installation Services in Land & Marine.</li>
+						</ul>
+					</div>
+					<div className="fourth-section-text2">
+						<span>Our Mission:</span>
+						<ul>
+							<li>
+								To provide quality service in the fastest possible manner with minimal impact on the environment.{" "}
+							</li>
+							<li>
+								To exceed our clients' trust through the delivery of quality and exclusive world class satisfactory
+								products & services.{" "}
+							</li>
+							<li>
+								To utilize the latest specialized equipment and experience personnel and tackle all land & marine
+								constructions.
+							</li>
+							<li>
+								To ensure minimal safety hazards on-site for personnels through the use of suitable equipments, experts
+								and safety equipments & measures.
+							</li>
+						</ul>
+					</div>
+					<div className="frame2">
+						<img src="assets/frame2.png" alt="frame2" />
+					</div>
+				</div>
 				<div>5</div>
 				<div>6</div>
 				<div>7</div>

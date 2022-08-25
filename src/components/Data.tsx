aa24c3b077693fb0acc90473b4f79dccad146d5c
Arigbo <arigbojesse75@gmail.com>
2022-08-24 12:30:37 -1200

    Create Data.tsx

---
+++ src/components/Data.tsx
@@ -0,0 +1,102 @@
+
+import React from "react"
+
+//====Products Data====
+export const Research = [
+    {
+        id: 1,
+        number: "25+",
+        text: "Years Experience"
+    },
+    {
+        id: 2,
+        number: "50+",
+        text: "Industries served."
+    },
+    {
+        id: 3,
+        number: "5+",
+        text: "Factories Built"
+    }
+]
+export const Project = [
+    {
+        id: 1,
+img: <img src={"/images/Rectangle1.png"} alt="" />
+    },
+    {
+        id: 1,
+img: <img src={"/images/Rectangle2.png"} alt="" />
+    },
+    {
+        id: 1,
+        img: <img src={"/images/Rectangle3.png"} alt="" />
+    }
+]
+export const OurSection1 = [
+    {
+        id:1,
+        head:"Our Vision:",
+        img: <img src={"/images/Frame89.png"} alt="" />
+    },
+
+]
+export const OurSection2=[
+    {
+        id:2,
+        head:"Our Mission:",
+        img: <img src={"/images/Frame78.png"} alt="" />
+    }
+]
+export const Why=[
+    {
+        id:1,
+        head:"Quality",
+        img: <img src={"/images/Badge.png"} alt="" />,
+        body:"Great service delivery is incomplete without quality. It is our core value of operation, we achieve this by employing the policy of harnessing available resources, experience (of higly skilled engineers) and technical expertise."
+    },
+    {
+        id:2,
+        head:"Integrity",
+        img: <img src={"/images/Union.png"} alt="" />,
+        body:"Service entails a lot of commitment laced with passion, with years of experience and service provision, we stand for excellent and honest work as we stay true to our words.Maintaining the trust placed in us by all our clients and stakeholders is key to us."
+    },
+    {
+        id:3,
+        head:"Professionalism",
+        img: <img src={"/images/Bag.png"} alt="" />,
+        body:"Skill and competence are as important as practice therefore all staff are highly trained, possess high experiential knowledge in the field, and are highly professional.We value professionalism, to us it means giving clients the best value for their money."
+    },
+    {
+        id:4,
+        head:"Safety",
+        img: <img src={"/images/Helmet.png"} alt="" />,
+        body:"As one of our offered service we are not lacking in ensuring that our installations and technical works are properly done.We have zero reservations as regards safety. Our staff are highly trained for personal & on-site safety. "
+    }  
+]
+export const Workers=[
+    {
+        id:1,
+        img: <img src={"/images/Portrait1.png"} alt="" />,
+        name:"Joy Julius",
+        ceo:"CEO",
+        iconinstagram:<i class="fa-brands fa-instagram"></i>,
+        icontwitter:	<i class="fa-brands fa-twitter" />
+    },
+    {
+        id:2,
+        img: <img src={"/images/Portrait3.png"} alt="" />,
+        name:"Gabriel Zaa",
+        ceo:"CEO",
+        iconinstagram:<i class="fa-brands fa-instagram"></i>,
+        icontwitter:<i class="fa-brands fa-twitter" />
+    },
+    {
+        id:3,
+        img: <img src={"/images/Portrait2.png"} alt="" />,
+        name:"Gabriel Zaa",
+        ceo:"CEO",
+        iconinstagram:<i class="fa-brands fa-instagram"></i>,
+        icontwitter:	<i class="fa-brands fa-twitter" />
+    }
+]

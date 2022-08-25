50eaabe75682306da304e7e4062da1cfc2e7a552
Arigbo <arigbojesse75@gmail.com>
2022-08-24 16:38:40 -1200

    Update projects.tsx

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -11,7 +11,7 @@ function Project() {
                     <div className="img">
                         <img src={'/images/Frame99.png'} alt="" className=""></img>
                     </div>
-                    <h1 className="h1">About
+                    <h1 className="h1">Project
                         <br />
                         Ralonik LTD.
                     </h1>
@@ -109,48 +109,48 @@ function Project() {
 
                         </div>
                     </div>
-        </section>
-        <section className="home-section-6">
-					<div className="section6-inner">
-						<h2>Clients Reviews</h2>
-						<p className="section6-inner-txt1">
-							{`I had been looking to get my shipment and maintenance done but couldn't find the right Construction Company to do so. Rolanick Construction has been exceptional at what they do, from pre-planning, procurement, constructing in itself and maintenance it has been worth it. They understood the job and they delivered! Thank you!`}
-						</p>
-						<div className="section6-inner-profile">
-							<img src="/assets/clientPic.png" alt="" />
-							<h3>Cindy Clifford</h3>
-							<p className="section6-inner-profile_caption">Creative Director Sobaz Oil and gas</p>
-						</div>
-						<div className="slider">
+                </section>
+                <section className="home-section-6">
+                    <div className="section6-inner">
+                        <h2>Clients Reviews</h2>
+                        <p className="section6-inner-txt1">
+                            {`I had been looking to get my shipment and maintenance done but couldn't find the right Construction Company to do so. Rolanick Construction has been exceptional at what they do, from pre-planning, procurement, constructing in itself and maintenance it has been worth it. They understood the job and they delivered! Thank you!`}
+                        </p>
+                        <div className="section6-inner-profile">
+                            <img src="/assets/clientPic.png" alt="" />
+                            <h3>Cindy Clifford</h3>
+                            <p className="section6-inner-profile_caption">Creative Director Sobaz Oil and gas</p>
+                        </div>
+                        <div className="slider">
                             <div className="slider1"></div>
                             <div className="slider2"></div>
                             <div className="slider3"></div>
                         </div>
-					</div>
-				</section>
+                    </div>
+                </section>
                 <section className="firth-section">
-                <h1>Our Team</h1>
-                <div className="profile">
-                    {
-                        Workers.map((item) => {
-                            return (
-                                <div className="inner-profile" key={item.id}>
-                                    <div className="Portrait">
-                                        {item.img}
-                                    </div>
-                                    <div className="info">
-                                        <h2 className="h2 profile-h2">{item.name}</h2>
-                                        <p className="profile-p">{item.ceo}</p>
-                                        <div className="icons">
-                                            {item.iconinstagram}
-                                            {item.icontwitter}
+                    <h1>Our Team</h1>
+                    <div className="profile">
+                        {
+                            Workers.map((item) => {
+                                return (
+                                    <div className="inner-profile" key={item.id}>
+                                        <div className="Portrait">
+                                            {item.img}
+                                        </div>
+                                        <div className="info">
+                                            <h2 className="h2 profile-h2">{item.name}</h2>
+                                            <p className="profile-p">{item.ceo}</p>
+                                            <div className="icons">
+                                                {item.iconinstagram}
+                                                {item.icontwitter}
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
+                                )
+                            })
+                        }
+                    </div>
                 </section>
             </div >
         </FrontLayout >

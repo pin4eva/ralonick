da490bfa888a5c909dfceecfb81b2c58e1fea7f7
Arigbo <arigbojesse75@gmail.com>
2022-08-25 15:09:16 -1200

    Update Data.tsx

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -41,14 +41,14 @@ export const Research2 = [
         text: "Years Experience"
     }
 ]
-export const Project = [
+export const Transforming = [
     {
         id: 1,
-img: <img src={"/images/Rectangle1.png"} alt="" />
+        img: <img src={"/images/Rectangle1.png"} alt="" />
     },
     {
         id: 2,
-img: <img src={"/images/Rectangle2.png"} alt="" />
+        img: <img src={"/images/Rectangle2.png"} alt="" />
     },
     {
         id: 3,
@@ -57,94 +57,123 @@ img: <img src={"/images/Rectangle2.png"} alt="" />
 ]
 export const OurSection1 = [
     {
-        id:1,
-        head:"Our Vision:",
+        id: 1,
+        head: "Our Vision:",
         img: <img src={"/images/Frame89.png"} alt="" />
     },
 
 ]
-export const OurSection2=[
+export const OurSection2 = [
     {
-        id:2,
-        head:"Our Mission:",
+        id: 2,
+        head: "Our Mission:",
         img: <img src={"/images/Frame78.png"} alt="" />
     }
 ]
-export const Why=[
+export const Why = [
     {
-        id:1,
-        head:"Quality",
+        id: 1,
+        head: "Quality",
         img: <img src={"/images/Badge.png"} alt="" />,
-        body:"Great service delivery is incomplete without quality. It is our core value of operation, we achieve this by employing the policy of harnessing available resources, experience (of higly skilled engineers) and technical expertise."
+        body: "Great service delivery is incomplete without quality. It is our core value of operation, we achieve this by employing the policy of harnessing available resources, experience (of higly skilled engineers) and technical expertise."
     },
     {
-        id:2,
-        head:"Integrity",
+        id: 2,
+        head: "Integrity",
         img: <img src={"/images/Union.png"} alt="" />,
-        body:"Service entails a lot of commitment laced with passion, with years of experience and service provision, we stand for excellent and honest work as we stay true to our words.Maintaining the trust placed in us by all our clients and stakeholders is key to us."
+        body: "Service entails a lot of commitment laced with passion, with years of experience and service provision, we stand for excellent and honest work as we stay true to our words.Maintaining the trust placed in us by all our clients and stakeholders is key to us."
     },
     {
-        id:3,
-        head:"Professionalism",
+        id: 3,
+        head: "Professionalism",
         img: <img src={"/images/Bag.png"} alt="" />,
-        body:"Skill and competence are as important as practice therefore all staff are highly trained, possess high experiential knowledge in the field, and are highly professional.We value professionalism, to us it means giving clients the best value for their money."
+        body: "Skill and competence are as important as practice therefore all staff are highly trained, possess high experiential knowledge in the field, and are highly professional.We value professionalism, to us it means giving clients the best value for their money."
     },
     {
-        id:4,
-        head:"Safety",
+        id: 4,
+        head: "Safety",
         img: <img src={"/images/Helmet.png"} alt="" />,
-        body:"As one of our offered service we are not lacking in ensuring that our installations and technical works are properly done.We have zero reservations as regards safety. Our staff are highly trained for personal & on-site safety. "
-    }  
-]
-export const Workers=[
-    {
-        id:1,
-        img: <img src={"/images/Portrait1.png"} alt="" />,
-        name:"Joy Julius",
-        ceo:"CEO",
-        iconinstagram:<i class="fa-brands fa-instagram"></i>,
-        icontwitter:	<i class="fa-brands fa-twitter" />
-    },
-    {
-        id:2,
-        img: <img src={"/images/Portrait3.png"} alt="" />,
-        name:"Gabriel Zaa",
-        ceo:"CEO",
-        iconinstagram:<i class="fa-brands fa-instagram"></i>,
-        icontwitter:<i class="fa-brands fa-twitter" />
-    },
-    {
-        id:3,
-        img: <img src={"/images/Portrait2.png"} alt="" />,
-        name:"Gabriel Zaa",
-        ceo:"CEO",
-        iconinstagram:<i class="fa-brands fa-instagram"></i>,
-        icontwitter:	<i class="fa-brands fa-twitter" />
+        body: "As one of our offered service we are not lacking in ensuring that our installations and technical works are properly done.We have zero reservations as regards safety. Our staff are highly trained for personal & on-site safety. "
     }
 ]
-export const Projects=[
+export const Workers = [
     {
-        id:1,
-        header:"Project Name",
-        text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mi sagittis aliquet elementum nunc lectus. Arcu, non ornare commodo, convallis lectus.",
-        link:"More Details",
-        arrow: <img src={"./images/VectorWhite.png"} alt="" />
-
+        id: 1,
+        img: <img src={"/images/Portrait1.png"} alt="" />,
+        name: "Joy Julius",
+        ceo: "CEO",
+        iconinstagram: <i class="fa-brands fa-instagram"></i>,
+        icontwitter: <i class="fa-brands fa-twitter" />
     },
     {
-        id:2,
-        header:"Project Name",
-        text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mi sagittis aliquet elementum nunc lectus. Arcu, non ornare commodo, convallis lectus.",
-        link:"More Details",
-        arrow: <img src={"./images/VectorWhite.png"} alt="" />
-
+        id: 2,
+        img: <img src={"/images/Portrait3.png"} alt="" />,
+        name: "Gabriel Zaa",
+        ceo: "CEO",
+        iconinstagram: <i class="fa-brands fa-instagram"></i>,
+        icontwitter: <i class="fa-brands fa-twitter" />
     },
     {
-        id:3,
-        header:"Project Name",
-        text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mi sagittis aliquet elementum nunc lectus. Arcu, non ornare commodo, convallis lectus.",
-        link:"More Details",
-        arrow: <img src={"./images/VectorWhite.png"} alt="" />
-
+        id: 3,
+        img: <img src={"/images/Portrait2.png"} alt="" />,
+        name: "Gabriel Zaa",
+        ceo: "CEO",
+        iconinstagram: <i class="fa-brands fa-instagram"></i>,
+        icontwitter: <i class="fa-brands fa-twitter" />
+    }
+]
+export const Projects = [
+    {
+        id: 1,
+        header: "Project Name",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mi sagittis aliquet elementum nunc lectus. Arcu, non ornare commodo, convallis lectus.",
+        link: "More Details",
+        arrow: <i class="fa-solid fa-square-arrow-up-right"></i>,
+        img: <img src={"./images/Project1.png"} alt="" />
+    },
+    {
+        id: 2,
+        header: "Project Name",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mi sagittis aliquet elementum nunc lectus. Arcu, non ornare commodo, convallis lectus.",
+        link: "More Details",
+        arrow: <i class="fa-solid fa-square-arrow-up-right"></i>,
+        img: <img src={"./images/Project3.png"} alt="" />
+    },
+    {
+        id: 3,
+        header: "Project Name",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mi sagittis aliquet elementum nunc lectus. Arcu, non ornare commodo, convallis lectus.",
+        link: "More Details",
+        arrow: <i class="fa-solid fa-square-arrow-up-right"></i>,
+        img: <img src={"./images/Project2.png"} alt="" />
+    }
+]
+export const Project= [
+    {
+        id: 1,
+        img: <img src={"./images/Project1.png"} alt="" />,
+        header: "Project Name",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mi sagittis aliquet elementum nunc lectus. Arcu, non ornare commodo, convallis lectus.",
+        link: "More Details",
+        arrow: <i class="fa-solid fa-square-arrow-up-right"></i>
+    
+    },
+    {
+        id: 2,
+        img: <img src={"./images/Project3.png"} alt="" />,
+        header: "Project Name",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mi sagittis aliquet elementum nunc lectus. Arcu, non ornare commodo, convallis lectus.",
+        link: "More Details",
+        arrow: <i class="fa-solid fa-arrow-up-right"></i>
+        
+    },
+    {
+        id: 3,
+        img: <img src={"./images/Project2.png"} alt="" />,
+        header: "Project Name",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mi sagittis aliquet elementum nunc lectus. Arcu, non ornare commodo, convallis lectus.",
+        link: "More Details",
+        arrow: <i class="fa-solid fa-arrow-up-right"></i>
+        
     }
 ]

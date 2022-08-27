f7c7f3e96e736df51e6b827f6d85a603776cfb99
Arigbo <arigbojesse75@gmail.com>
2022-08-26 13:03:31 -1200

    Create second.tsx

---
+++ src/pages/projects/[id]/second.tsx
@@ -0,0 +1,24 @@
+// import React from "react";
+// import { Project2 } from "../../../components/Data";
+// import { useRouter } from "next/router";
+// import FrontLayout from "../../../layouts/front.layout";
+// function SingleProjects(){
+//     const router=useRouter();
+//     const {id} = router.query;
+//     return(
+       
+//         <FrontLayout>
+//         <div>
+//             {Project2.map((item)=>{
+//                 return(
+//                     <div key={item.id}>
+//              <p>learn</p>
+//                         {item.header}
+//                     </div>
+//                 )
+//             })}
+//         </div>
+//         </FrontLayout>
+//     )
+// }
+// export default SingleProjects

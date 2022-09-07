0e53bb5d01f2356a7b5aa00620684688c5dea3b0
Peter Akaliro <pin4eva@hotmail.com>
2022-09-07 17:45:56 +0100

    added title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,18 @@ import type { AppProps } from "next/app";
 import "../styles/index.scss";
 import "../styles/custom.scss";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { Fragment } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<Fragment>
+			<Head>
+				<title>Ralonick</title>
+			</Head>
+			<Component {...pageProps} />
+		</Fragment>
+	);
 }
 
 export default MyApp;

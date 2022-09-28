13119c0583d69cc569531bfeb6104bd185782a70
stephen4018 <stephenohaeri4018@gmail.com>
2022-09-28 15:49:04 +0100

    added custom style to bootstrap container class to remove padding-inline

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -52,7 +52,7 @@ const AboutPage = () => {
 				<section className="transforming container">
 					<div className="transforming-inner">
 						<div className="transforming-inner-left " data-aos="zoom-in-up" data-aos-duration="3000">
-							<p className="text-danger">ROLANICK</p>
+							<p className="text-danger fw-600">ROLANICK</p>
 							<p className="transforming-heading">Who We Are...</p>
 							<h2>Transforming With Innovations</h2>
 							<p className="transforming-paragraph">
